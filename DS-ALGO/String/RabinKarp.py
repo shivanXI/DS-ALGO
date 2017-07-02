6fdca0706ceae514673095cc91498f3f502800fb
shivanXI <shivanstar7@gmail.com>
2017-07-02 17:22:54 +0530

    Added String algorithm practice probs-03 kurumanchi

--- DS-ALGO/String/RabinKarp.py
+++ DS-ALGO/String/RabinKarp.py
@@ -47,3 +47,17 @@ def prefixTable(pattern):
 			k = k+1
 		F[q] = k
 	return F
+def KMP(text, pattern):
+	n = len(text)
+	m = len(pattern)
+	F = prefixTable(pattern)	
+	q = 0
+	for i in range(n):
+		while q > 0 and pattern[q] != text[i]:
+			q = F[q-1]
+		if pattern[q] == text[i]:
+			q = q + 1
+		if q == m:
+			return i - m + 1
+	return -1
+	
