b22ae2303e521801f4004658490c3a6d1cf2bc4c
shivanXI <shivanstar7@gmail.com>
2017-07-11 17:11:05 +0530

    Added new problem of tree traversal-03 using 2 queue-02.

--- DS-ALGO/Tree/levelordernextline2queues.cpp
+++ DS-ALGO/Tree/levelordernextline2queues.cpp
@@ -7,6 +7,31 @@ struct Node
 	Node *left, *right;
 };
 
+void levelOrder2queue(Node *root)
+{
+	queue<Node *> q1, q2;
+
+	if(root == NULL)
+		return;
+
+	q1.push(root);
+	while (!q1.empty() || !q2.empty())
+	{
+		while(!q1.empty())
+		{
+			if (q1.front()->left != NULL)
+				q2.push(q1.front()->left);
+			if (q1.front()->right != NULL)
+				q2.push(q1.front()->right);
+
+			cout << q1.front()->data <<" ";
+			q1.pop();
+		}
+
+		cout << "\n";
+	}
+}
+
 Node* newNode (int data)
 {
 	Node *temp = new Node;
