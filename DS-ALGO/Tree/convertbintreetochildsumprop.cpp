5710294c7e423f9f39fbcba2b84aa71932f53124
shivanXI <shivanstar7@gmail.com>
2017-07-11 19:52:25 +0530

    Added conversion of binary tree to support child sum property-01.

--- DS-ALGO/Tree/convertbintreetochildsumprop.cpp
+++ DS-ALGO/Tree/convertbintreetochildsumprop.cpp
@@ -16,5 +16,24 @@ void convertTree(struct node* node)
 {
 	int left_data = 0, right_data = 0, diff;
 
-	if ()
+	if (node == NULL || (node->left == NULL && node-> right == NULL))
+		return;
+	else
+	{
+		convertTree(node->left);
+		convertTree(node->right);
+
+		if(node->left != NULL)
+			left_data = node->left->data;
+		if(node->right != NULL)
+			right_data = node->right->data;
+
+		diff = left_data + right_data - node->data;
+
+		if (diff > 0)
+			node->data = node->data + diff;
+		if (diff < 0)
+			increment(node, -diff);
+	}
 }
+
