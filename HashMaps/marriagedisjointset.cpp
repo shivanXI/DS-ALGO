ea51f335189d6dc88150aa1d3c5da721158fd631
shivanXI <shivanstar7@gmail.com>
2017-07-23 19:09:11 +0530

    added disjoint set marriage unique friends HE-02

--- HashMaps/marriagedisjointset.cpp
+++ HashMaps/marriagedisjointset.cpp
@@ -9,3 +9,23 @@ int root(int array_l[], int i){
 	return i;
 }
 
+int main()
+{
+	int x,y,a,b,q;
+
+	long result = 0;
+	cin>>x>>y;
+
+	int array_l[x+y], size_l[x+y], size_Male[x+y], size_Female[x+y];
+	for(int i=0;i<x+y;i++){
+		array_l[i] = i;
+		size_l[i] = 1;
+		if(i<x){
+			size_Male[i] = 1;
+			size_Female[i] = 0;
+		}else{
+			size_Male[i] = 0;
+			size_Female[i] = 1;
+		}
+	} 
+}
