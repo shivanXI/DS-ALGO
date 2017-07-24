b00274d6119a06c29b4187d4eaf815ed514f3032
shivanXI <shivanstar7@gmail.com>
2017-07-24 15:10:00 +0530

    added disjoint set teacher's dilemma-03

--- HashMaps/teacherdillemadisjointset.cpp
+++ HashMaps/teacherdillemadisjointset.cpp
@@ -38,47 +38,13 @@ void make_union(int array_l[], int size_l[], int size_Male[], int size_Female[],
 
 int main()
 {
-	int x,y,a,b,q;
+	ll int n,m;
 
-	long result = 0;
-	cin>>x>>y;
+	//long result = 0;
+	cin>>n>>m;
+	init(n);
+	ll int relations = m;
 
-	int array_l[x+y], size_l[x+y], size_Male[x+y], size_Female[x+y];
-	for(int i=0;i<x+y;i++){
-		array_l[i] = i;
-		size_l[i] = 1;
-		if(i<x){
-			size_Male[i] = 1;
-			size_Female[i] = 0;
-		}else{
-			size_Male[i] = 0;
-			size_Female[i] = 1;
-		}
-	}
-
-	cin>>q;
-	for(int i=0; i<q; i++){
-		cin>>a>>b;
-		make_union(array_l, size_l, size_Male, size_Female, a-1, b-1);
-	}
-
-	cin>>q;
-	for(int i=0; i<q; i++){
-		cin>>a>>b;
-		make_union(array_l, size_l, size_Male, size_Female, x+a-1, x+b-1);
-	}
-
-	cin>>q;
-	for(int i=0; i<q; i++){
-		cin>>a>>b;
-		make_union(array_l, size_l, size_Male, size_Female, a-1, x+b-1);
-	}
-
-	for(int i=0; i<x+y; i++){
-		if(array_l[i] == i){
-			result += size_Male[i]*(long)(y-size_Female[i]);
-		}
-	}
-	cout<<result;
+	
 	return 0;
 }
