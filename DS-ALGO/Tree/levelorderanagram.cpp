6e4b6715bc63d987092e71c145550728de56f5f3
shivanXI <shivanstar7@gmail.com>
2017-07-26 00:54:53 +0530

    Anagram level by level

---
+++ DS-ALGO/Tree/levelorderanagram.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct node *left, *right;
+};
+
+bool test_anagrams(Node *root1, Node *root2)
+{
+	if(root1 == NULL && root2 == NULL)
+		return true;
+	if(root1 == NULL || root2 == NULL)
+		return false;
+
+	queue<Node *> q1, q2;
+	q1.push(root1);
+	q2.push(root2);
+
+	while (1)
+	{
+		int n1 = q1.size(), n2 = q2.size();
+
+		if(n1 != n2)
+			return false;
+		
+		if(n1 == 0)
+			break;
+
+		vector<int> curr_level1, curr_level2;
+		while (n1 > 0)
+		{
+			Node *node1 = q1.front();
+			q1.pop();
+			if(node1->left != NULL)
+				q1.push(node1->left);
+			if(node1->right != NULL)
+				q1.push(node1->right);
+			n1--;
+
+			Node *node2 = q2.front();
+			q2.pop();
+			if(node2->left != NULL)
+				q2.push(node2->left);
+			if(node2->right != NULL)
+				q2.push(node2->right);
+
+			curr_level1.push_back(node1->data);
+			curr_level2.push_back(node2->data);
+		}
+
+		sort(curr_level1.begin(), curr_level1.end());
+		
+	}
+}
