54c84bba4232e556538bfbe5ab2345884582dbea
shivanXI <shivanstar7@gmail.com>
2017-07-26 18:48:15 +0530

    negative in current window-01

--- DS-ALGO/Queue/k-windowsizeneg.cpp
+++ DS-ALGO/Queue/k-windowsizeneg.cpp
@@ -5,5 +5,22 @@ using namespace std;
 void findFirstNegativeInteger(int array_l[], int n, int k)
 {
 	deque<int> ndeck;
-	
+	int i;
+	for(i = 0; i < k; i++)
+		if(array_l[i] < 0)
+			ndeck.push_back(i);
+
+	for( ; i<n; i++)
+	{
+		if(!ndeck.empty())
+			cout << array_l[ndeck.front()] << " ";
+		else
+			cout << "0" << " ";
+		while ( (!ndeck.empty() ) && ndeck.front() < (i - k + 1))
+			ndeck.pop_front();
+		if (array_l[i] < 0)
+			ndeck.push_back(i);
+	}
+
+
 }
