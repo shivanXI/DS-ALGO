9965b053541e9fe44a450ac3945ae207a0ec55ce
shivanXI <shivanstar7@gmail.com>
2017-08-04 01:25:27 +0530

    Tree section left child sum leaf added-01

---
+++ DS-ALGO/Tree/treeleftchildsumleaf.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int key;
+	struct Node* left, *right;
+};
+
+Node *newNode(char k)
+{
+	Node *node = new Node;
+	node->key = k;
+	node->right = node->left = NULL;
+	return node;
+}
+
+bool isLeaf(Node *node)
+{
+	if(node == NULL)
+		return false;
+	if (node->left == NULL && node->right == NULL)
+		return true;
+	return false;
+}
+
+int leftLeavesSum(Node *root)
+{
+	int result = 0;
+
+	if(root != NULL)
+	{
+		if(isLeaf(root->left))
+			result += root->left->key;
+		else
+			result += leftLeavesSum(root->left);
+
+	}
+
+	return result;
+}
