cb567e911e65a371d76ff3b51a470bbced297956
shivanXI <shivanstar7@gmail.com>
2017-08-06 12:07:22 +0530

    Added hashmaps in cpp-02

--- HashMaps/firsthash.cpp
+++ HashMaps/firsthash.cpp
@@ -5,25 +5,29 @@ int main()
 {
 	int t;
 	string initial_check = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string str;
 	unordered_map<char, int> mp;
 	for (int i = 0 ; i < initial_check.size(); i++)
 	{
 		mp[initial_check[i]] = i;
 	}	
+
 	cin >> t;
-	getline(cin, str)
+	
+	getline(cin, str);
 	while(t--){
 		int index = 0, n = 1, hash = 0;
 		getline(cin, str);
 		for(char ch : str){
-			if( ch == " "){
+			if( ch == ' '){
 				index = 0;
 				n += 1;
 			}else{
 				hash += index + mp[ch];
 				index++ ;
 			}
-		} cout << hash*n << endl;
+		} 
+		cout << hash*n << endl;
 	}
 	return 0;
 }
