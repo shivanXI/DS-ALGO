613b4e2577b91eaf010775babd7b9ff5276e9265
shivanXI <shivanstar7@gmail.com>
2017-09-10 23:32:16 +0530

    Placement practice continues-05

---
+++ DP/fillblanks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int grid[10][10];
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			cin>>grid[i][j];
+		}
+	}
+
+
+	return 0;
+}
