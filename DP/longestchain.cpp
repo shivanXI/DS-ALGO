545889cf81a570c4a41f6bb122d3265b4a584b9a
shivanXI <shivanstar7@gmail.com>
2017-09-12 17:49:40 +0530

    Placement practice continues-11

---
+++ DP/longestchain.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+	int findLongestChain(vector<vector<int>>&pairs){
+		sort(pairs.begin(), pairs.end(), cmp);
+		int count = 0;
+		vector<int>&pair = pairs[0];
+		for(int i=0;i<pairs.size();i++)
+		{
+			if(i=0 || pairs[i][0] > pair[1])
+			{
+				pair = pairs[i];
+				count++;
+			}
+		}
+		return count;
+	}
+private:
+	static bool cmp(vector<int>&a, vector<int>&b)
+	{
+		return a[1] < b[1];
+	}
+};
